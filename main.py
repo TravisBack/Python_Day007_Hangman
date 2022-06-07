838a5f4bd1916d64c078492dbd9a4cac39014b0f
travisback <travisback@icloud.com>
2022-06-06 17:02:46 -0700

    added visual spaces for word len

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import random
 
 word_list = ["ardvark", "baboon", "camel"]
 
-# Choose a random word
 chosen_word = random.choice(word_list)
+letters_to_find = len(chosen_word)
+strikes = 0
 
-#  Ask the user to guess a letter
-guess = input("Guess a letter: ").lower()
+#  print(f"DEBUG: {chosen_word}")
+display = []
 
-# Check if the letter guessed is in the chosen word.
-for letter in chosen_word:
-    if letter == guess:
-        print(True)
-    else:
-        print(False)
+for _ in range(letters_to_find):
+    display.append('_')
+
+print(display)
+
+while letters_to_find > 0:
+    guess = input("Guess a letter: ").lower()
+
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guess:
+            display[i] = chosen_word[i]
+            letters_to_find -= 1
+
+    print(display)
